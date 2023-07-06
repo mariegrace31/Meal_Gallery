bee8646ab474b3ce25c38fc0b9372d43ba7dbf86
lily4178993 <nellytelli@gmail.com>
2023-07-06 10:57:46 +0000

    Add app key for Involvement API

--- src/app/utils/api/key.js
+++ src/app/utils/api/key.js
@@ -1,3 +1,4 @@
 const mealGalleryKey = '1';
+const mealGalleryLikeKey = '9eR6rn0eO8bc23';
 
-export { mealGalleryKey as default };
+export { mealGalleryKey as apiKey, mealGalleryLikeKey as apiLikeKey };
